bf7d475e74336a04db4f7bf8dffd03bd9aee197e
Paulo de Tarso <paulobitas@gmail.com>
2025-12-08 13:26:22 -0300

    Update OrganizeFiles.py
    Add "move folder"

--- OrganizeFiles.py
+++ OrganizeFiles.py
@@ -7,7 +7,12 @@ def _cleanScreen():
     pass
 
 new_Folder = "OrganizadorDeArquivos"
+
 path = "C:\\Users\\paulo.oliveira\\Desktop\\Python\\Teste"
+
+print("Original Folder: ", os.getcwd())
+os.chdir(path)
+print("Current folder: ", os.getcwd())
 
 
 if(not os.path.exists(new_Folder)):
@@ -28,6 +33,6 @@ for files in os.listdir():
                 print("tsete")
                 print(f"Folder {extension} created sucessfully!")
             #os.replace(files, f"{new_Folder}/{extension}/{files}")
-            #print(f"File {files} moved to folder {extension} successfully!")    
-    
-    
+            #print(f"File {files} moved to folder {extension} successfully!")
+            else:
+                print(f"Folder {extension} already exists.")    
