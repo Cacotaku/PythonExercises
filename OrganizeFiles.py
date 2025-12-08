69ef3eb9392261ce37dd2d0fe7c0b65cd3cf2a4c
Paulo de Tarso <paulobitas@gmail.com>
2025-12-08 16:07:44 -0300

    Update OrganizeFiles.py
    Add option to change directory

--- OrganizeFiles.py
+++ OrganizeFiles.py
@@ -10,29 +10,67 @@ new_Folder = "OrganizadorDeArquivos"
 
 path = "C:\\Users\\paulo.oliveira\\Desktop\\Python\\Teste"
 
-print("Original Folder: ", os.getcwd())
+_cleanScreen()
+
+print(f"Default directory: {path}")
+
+option_input = input(f"Do you want to change the directory? (y/n)").lower().strip()
+
+while(option_input not in ['y', 'n']):
+
+    option_input = input(f"Invalid option! Do you want to change the directory? (y/n)").lower().strip()
+
+if(option_input == 'y'):
+    
+    path = input(f"""Type the new directory path: 
+                 Exemple: {os.getcwd()}""").strip()
+    
+    if(os.path.exists(path) is True):
+
+        print(f"Path {path} found!")
+
+    else:
+
+        while(os.path.exists(path) is False):
+        
+            print(f"Path {path} does not exists!")
+            path = input(f"""Type the new directory path: 
+                Exemple: {os.getcwd()}""").strip()
+        
+
 os.chdir(path)
-print("Current folder: ", os.getcwd())
+print(f"Current folder: {os.getcwd()}")
 
 
 if(not os.path.exists(new_Folder)):
+    
     os.mkdir(new_Folder)
     print(f"Folder {new_Folder} created sucessfully!")
 
 else:
+    
     print(f"Folder {new_Folder} already exists.")
 
 for files in os.listdir():
+    
     print(files)
+    
     if os.path.isfile(files):
+
         extension = files.split(".")[-1]
+
         if ("." in files):
 
             if(not os.path.exists(f"{new_Folder}//{extension}")):
+
                 os.mkdir(f"{new_Folder}//{extension}")
-                print("tsete")
                 print(f"Folder {extension} created sucessfully!")
-            #os.replace(files, f"{new_Folder}/{extension}/{files}")
-            #print(f"File {files} moved to folder {extension} successfully!")
+
             else:
+
                 print(f"Folder {extension} already exists.")    
+            
+            os.replace(files, f"{new_Folder}/{extension}/{files}")
+            print(f"File {files} moved to folder {extension} successfully!")
+    
+    
